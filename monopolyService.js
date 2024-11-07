9a81783e67fa96a587bfa42836ea121850f4a807
= <iw3@calvin.edu>
2024-11-06 22:31:20 -0500

    work

--- monopolyService.js
+++ monopolyService.js
@@ -42,7 +42,7 @@ const db = pgp({
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const router = express.Router();
 router.use(express.json());
 
